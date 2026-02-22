ec83bc20856d41bb8c3c9a35ffec114ca69a8725
Eduardo Samano <esamano@csilvm-08.cs.ucsb.edu>
2026-02-22 11:21:42 -0800

    Completed code for vertical.cpp

--- vertical.cpp
+++ vertical.cpp
@@ -7,12 +7,32 @@
 using namespace std;
 
 // Declare printV() here
+void printV(long int num);
 
 int main() {
+
     // 1. Get user input
+    long int userInput;
+    cout << "Enter integer: ";
+    cin >> userInput;
+
     // 2. Check input and pass it on to printV()
+    if (userInput < 0){
+        cout << "Input must be a positive integer.\n";
+    }
+    else{
+        printV(userInput);
+    }
 
     return 0;
 }
 
 // Define printV() here
+void printV(long int num){
+    if (num < 10){
+        cout << num << endl;
+        return; 
+    }
+    printV(num/10);
+    cout << num % 10 << endl;
+}
