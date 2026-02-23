aa342d596a85a0dec9bb0b6906a0844a66c7621a
esamano1 <esamano@ucsb.edu>
2026-02-22 22:27:07 -0800

    Update sfunctions.cpp

--- sfunctions.cpp
+++ sfunctions.cpp
@@ -7,18 +7,33 @@ using namespace std;
 
 #include "sheaders.h"
 
-// DEFINE ALL 5 FUNCTIONS PER THE sheaders.h FILE HERE
-// Add Pre- and Post-Condition comments
+// Pre-conditions:
+//     inf is an ifstream object
+//     fname is the name of a file containing integers
+// Post-conditions:
+//     Opens fname, counts how many integers are in the file, closes the file
+//     Returns the number of integers found
 int getFileSize(ifstream& inf, string fname){
+    // Declare temp, and initialize size to 0
     int size(0), temp;
+
+    // Open the file and count how many integers can be read
     inf.open(fname);
     while (inf >> temp){
         size++;
     }
+
+    // Close file before returning
     inf.close();
     return size;
 }
 
+// Pre-conditions:
+//     in is an ifstream object
+//     fname is the name of a file containing 'size' number of integers
+//     arr has capacity for of 'size'
+// Post-conditions:
+//     Opens fname, reads 'size' integers into arr[], closes the file
 void getArray(ifstream& in, string fname, int arr[], int size){
     in.open(fname);
     for (int i = 0; i < size; i++){
@@ -27,18 +42,42 @@ void getArray(ifstream& in, string fname, int arr[], int size){
     in.close();
 }
 
+// Pre-conditions:
+//     a is an array
+//     number_used should be the size of the array
+//     index should be 0
+//     desc == true  -> sort in descending order
+//     desc == false -> sort in ascending order
+// Post-conditions:
+//     Sorts the array recursively in descending or ascending order
 void sort(bool desc, int a[], int number_used, int index){
+    // Base case: no elements left to sort
     if (number_used == 0){
         return;
     }
 
+    // Find the index of the value that should go at a[index]
+    // then swap it into place
     swap_values(a[index], a[find_index_of_swap(desc, a, index, number_used)]);
+    
+    // Recursively sort the rest of the array
     sort(desc, a, number_used - 1, index + 1);
 }
 
+// Pre-conditions:
+//     a is a an array
+//     start_index is the first index in the array being searched
+//     number_used is the number of elements after a[start_index]
+//     desc determines search mode:
+//       true  -> find index of maximum value
+//       false -> find index of minimum value
+// Post-conditions:
+//     Returns the index of the max/min element in the range:
+//       a[start_index] through a[start_index + number_used - 1]
 int find_index_of_swap(bool desc, int a[], int start_index, int number_used){
     int swap_index = start_index;
      if (desc){
+        // Descending sort: find index of the largest value in the current subarray
         int max = a[start_index];
         for (int i = start_index; i < start_index + number_used; i++){
             if (a[i] > max){
@@ -49,6 +88,7 @@ int find_index_of_swap(bool desc, int a[], int start_index, int number_used){
 
      }
      else{
+        // Ascending sort: find index of the smallest value in the current subarray
         int min = a[start_index];
         for (int i = start_index; i < start_index + number_used; i++){
             if (a[i] < min){
@@ -61,6 +101,10 @@ int find_index_of_swap(bool desc, int a[], int start_index, int number_used){
      return swap_index;
 }
 
+// Pre-conditions:
+//     v1 and v2 are valid int references
+// Post-conditions:
+//     The values in v1 and v2 are exchanged
 void swap_values(int& v1, int& v2){
     int temp = v1;
     v1 = v2;
